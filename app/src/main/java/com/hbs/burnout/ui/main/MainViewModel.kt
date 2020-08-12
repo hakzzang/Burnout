6664dc4a568dbb2673800f7c92cf5101c6714ea5
hakzzang <mbh930312@naver.com>
2020-08-12 13:33:58 +0900

    Main 관련 로직 추가

---
+++ app/src/main/java/com/hbs/burnout/ui/main/MainViewModel.kt
@@ -0,0 +1,8 @@
+package com.hbs.burnout.ui.main
+
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.ViewModel
+
+class MainViewModel @ViewModelInject constructor() : ViewModel() {
+
+}
