75f584dfa19d33c1741af81061093811bb8e7186
ggujangi <asmwj3@naver.com>
2020-08-23 20:32:24 +0900

    이미지 저장 Dialog 생성

--- app/src/main/java/com/hbs/burnout/ui/share/ShareViewModel.kt
+++ app/src/main/java/com/hbs/burnout/ui/share/ShareViewModel.kt
@@ -7,10 +7,29 @@ import androidx.lifecycle.ViewModel
 import com.hbs.burnout.model.ShareResult
 
 class ShareViewModel @ViewModelInject constructor() : ViewModel() {
-    var _shareData:MutableLiveData<ShareResult> = MutableLiveData();
-    val shareData:LiveData<ShareResult> = _shareData;
+    var _shareData:MutableLiveData<ShareResult> = MutableLiveData()
+    val shareData:LiveData<ShareResult> = _shareData
+
+    var _snsType:MutableLiveData<SnsType> = MutableLiveData()
+    val snsType:LiveData<SnsType> = _snsType
+
+    var tagSelected:MutableLiveData<Boolean> = MutableLiveData(true)
+
 
     fun updateShareData(data:ShareResult){
         _shareData.value = data;
     }
+
+    fun shareToSns(type:SnsType){
+        _snsType.value = type
+    }
+
+    fun updateTagSelected(isSelected:Boolean){
+        tagSelected.value = isSelected
+    }
 }
+
+enum class SnsType{
+    KAKAOTALK, FACEBOOK, INSTAGRAM
+}
+
