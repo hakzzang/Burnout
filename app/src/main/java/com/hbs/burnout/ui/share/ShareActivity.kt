8e1a95d676dc585136355149abfbf850890c1035
ggujangi <asmwj3@naver.com>
2020-08-23 23:30:48 +0900

    인스타 그램 공유하기 기능 추가

--- app/src/main/java/com/hbs/burnout/ui/share/ShareActivity.kt
+++ app/src/main/java/com/hbs/burnout/ui/share/ShareActivity.kt
@@ -1,23 +1,29 @@
 package com.hbs.burnout.ui.share
 
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.view.Window
 import androidx.activity.viewModels
+import androidx.core.graphics.drawable.toBitmap
 import androidx.lifecycle.Observer
 import com.google.android.material.transition.platform.MaterialContainerTransformSharedElementCallback
 import com.hbs.burnout.core.BaseActivity
 import com.hbs.burnout.databinding.ActivityShareBinding
 import com.hbs.burnout.model.ShareResult
+import com.hbs.burnout.ui.save.SaveDialog
+import com.hbs.burnout.utils.FileUtils
 
 class ShareActivity : BaseActivity<ActivityShareBinding>() {
+
+    private lateinit var uri: Uri
+
     private val viewModel by viewModels<ShareViewModel>()
     private val progressAdapter = ProgressAdapter()
 
     override fun bindBinding() = ActivityShareBinding.inflate(layoutInflater)
 
     override fun isUseTransition(): Boolean = false
-
 
     override fun preTransitionLogic() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
@@ -31,13 +37,30 @@ class ShareActivity : BaseActivity<ActivityShareBinding>() {
         TODO("Not yet implemented")
     }
 
+
+    private fun observe() {
+        viewModel.shareData.observe(
+            this,
+            Observer { data ->
+                run {
+                    progressAdapter.submitList(data.resultList)
+                    uri = FileUtils.saveImageToExternalFilesDir(
+                        this,
+                        binding.shareImage.drawable.toBitmap()
+                    )
+                }
+            })
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding.lifecycleOwner = this
 
         binding.viewModel = viewModel
         binding.handler = this
 
         initView(binding)
+
         observe();
     }
 
@@ -45,13 +68,27 @@ class ShareActivity : BaseActivity<ActivityShareBinding>() {
         binding.shareImage.clipToOutline = true
         binding.progressList.adapter = progressAdapter
 
-        val sample:ShareResult = ShareResult("새우버거 발닦기", "", "새우버거 발닦기 성공~\n더 친해지면 양치도 도전해보아요~~!")
-        sample.resultList = mutableListOf(ShareResult.Result("포챠펭", 85), ShareResult.Result("비둘기", 12), ShareResult.Result("돼지", 3))
+        val sample: ShareResult = ShareResult("새우버거 발닦기", "", "새우버거 발닦기 성공~\n더 친해지면 양치도 도전해보아요~~!")
+        sample.resultList = mutableListOf(
+            ShareResult.Result("포챠펭", 85),
+            ShareResult.Result("비둘기", 12),
+            ShareResult.Result("돼지", 3)
+        )
 
         viewModel.updateShareData(sample)
-    }
 
-    private fun observe(){
-        viewModel.shareData.observe(this, Observer { data-> progressAdapter.submitList(data.resultList)})
+        binding.fabShare.setOnClickListener {
+            val data = viewModel.shareData.value
+
+            val dialog = SnsDialog.newInstance(data!!.title, uri)
+            dialog.show(supportFragmentManager, "SNS_DIALOG")
+        }
+
+        binding.fabSave.setOnClickListener {
+            val data = viewModel.shareData.value
+
+            val dialog = SaveDialog.newInstance(data!!.title)
+            dialog.show(supportFragmentManager, "SAVE_DIALOG")
+        }
     }
 }
--- app/src/main/java/com/hbs/burnout/ui/share/SnsDialog.kt
+++ app/src/main/java/com/hbs/burnout/ui/share/SnsDialog.kt
@@ -2,19 +2,73 @@ package com.hbs.burnout.ui.share
 
 import android.app.Dialog
 import android.graphics.Color
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.viewModels
-import com.google.android.material.R
+import androidx.lifecycle.Observer
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.hbs.burnout.R
 import com.hbs.burnout.databinding.DialogSnsBinding
+import com.hbs.burnout.utils.IntentUtils
 
-class SnsDialog : BottomSheetDialogFragment() {
+class SnsDialog() : BottomSheetDialogFragment() {
 
     private val viewModel by viewModels<ShareViewModel>()
+
+    private val instagramLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            // do something here
+        }
+
+
+    var uri: Uri? = null
+    var title: String? = ""
+
+    companion object {
+        private val EXTRA_TITLE = "EXTRA_TITLE"
+        private val EXTRA_URI = "EXTRA_URI"
+
+        fun newInstance(title: String, uri: Uri): SnsDialog {
+            val args = Bundle()
+            args.putString(EXTRA_TITLE, title)
+            args.putString(EXTRA_URI, uri.toString())
+
+            val fragment = SnsDialog()
+            fragment.arguments = args
+            return fragment
+        }
+    }
+
+    private fun observe() {
+        viewModel.snsType.observe(requireActivity(), Observer { type ->
+            when (type) {
+                SnsType.KAKAOTALK -> {
+                    Toast.makeText(context, "준비중,,, (_ _)", Toast.LENGTH_SHORT).show()
+                }
+                SnsType.FACEBOOK -> {
+                    Toast.makeText(context, "준비중,,, (_ _)", Toast.LENGTH_SHORT).show()
+                }
+                SnsType.INSTAGRAM -> {
+                    val intent = IntentUtils.createInstagramFeedIntent(context, uri)
+                    instagramLauncher.launch(intent)
+                }
+            }
+            dismiss()
+        })
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        title = arguments?.getString(EXTRA_TITLE, "")
+        uri = Uri.parse(arguments?.getString(EXTRA_URI, ""))
+    }
+
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
 
@@ -41,9 +95,12 @@ class SnsDialog : BottomSheetDialogFragment() {
         savedInstanceState: Bundle?
     ): View? {
         val binding = DialogSnsBinding.inflate(inflater, container, false)
+        binding.lifecycleOwner = viewLifecycleOwner
 
         binding.viewModel = viewModel
         binding.handler = this
+
+        observe()
 
         return binding.root
     }
