bba68cd1423a392ca6bbec25fc9192732f031f11
hakzzang <mbh930312@naver.com>
2020-08-27 13:26:40 +0900

    ui 로직에서 미션 실행 시에, 숏컷추가하도록 변경

--- app/src/main/java/com/hbs/burnout/ui/main/MainActivity.kt
+++ app/src/main/java/com/hbs/burnout/ui/main/MainActivity.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 import android.text.Spannable
 import android.text.SpannableString
 import android.text.style.ForegroundColorSpan
-import android.util.Log
 import android.view.View
 import android.view.Window
 import androidx.activity.viewModels
