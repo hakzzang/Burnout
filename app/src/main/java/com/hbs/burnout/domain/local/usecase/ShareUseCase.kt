1b49f28b69ea7ce64c9e544470f4e6da39924dc6
hakzzang <mbh930312@naver.com>
2020-08-28 12:41:32 +0900

    ShareResult를 저장하는 로직 추가 구성

--- app/src/main/java/com/hbs/burnout/domain/local/usecase/ShareUseCase.kt
+++ app/src/main/java/com/hbs/burnout/domain/local/usecase/ShareUseCase.kt
@@ -1,21 +1,34 @@
 package com.hbs.burnout.domain.local.usecase
 
+import android.util.Log
 import com.hbs.burnout.domain.local.repository.ShareRepository
+import com.hbs.burnout.domain.local.repository.StageRepository
 import com.hbs.burnout.model.ShareResult
+import com.hbs.burnout.model.StageProgress
 import javax.inject.Inject
 
 interface ShareUseCase {
     suspend fun loadShareData(round: Int): ShareResult
 
-    fun saveShareData(stage: ShareResult): Long
+    suspend fun saveShareData(shareResult: ShareResult): Long
 }
 
 class ShareUseCaseImpl @Inject constructor(
+    private val stageRepository: StageRepository,
     private val shareRepository: ShareRepository
 ) : ShareUseCase {
 
     override suspend fun loadShareData(round: Int): ShareResult =
         shareRepository.getShareData(round)
 
-    override fun saveShareData(shareResult: ShareResult): Long = shareRepository.insert(shareResult)
+    override suspend fun saveShareData(shareResult: ShareResult): Long {
+        stageRepository.loadMission().mapIndexed {index, stage->
+            if(stage.progress == StageProgress.PLAYING){
+                shareResult.round = index + 1
+            }
+        }
+        Log.d("shareResult-round", shareResult.round.toString())
+        Log.d("shareResult-object", shareResult.toString())
+        return shareRepository.insert(shareResult)
+    }
 }
