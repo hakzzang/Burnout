3ad401da685debb858885b9caec6be0818f4e608
hakzzang <mbh930312@naver.com>
2020-08-28 23:36:08 +0900

    3스테이지까지만 진행할 수 있도록 로직 변경

--- app/src/main/java/com/hbs/burnout/ui/chat/ChattingViewModel.kt
+++ app/src/main/java/com/hbs/burnout/ui/chat/ChattingViewModel.kt
@@ -42,9 +42,9 @@ class ChattingViewModel @ViewModelInject constructor(
         val script = chattingUseCase.readNextScriptLine(scriptNumber) {
             _completedStage.value = Event(Unit)
         } ?: return
-        if(script.event == 4 || script.event == 6){
+        if (script.event == 4 || script.event == 6) {
 
-        }else{
+        } else {
             _readingScript.value = Event(script)
         }
     }
@@ -56,7 +56,6 @@ class ChattingViewModel @ViewModelInject constructor(
             }, { lastScript ->
                 chattingUseCase.saveScript(lastScript)
                 viewModelScope.launch(viewModelScope.coroutineContext + Dispatchers.Main) {
-                    Log.d("emit-event-lastscript",lastScript.event.toString())
                     _completedReadingScript.value = Event(lastScript)
                     _lastScript.value = lastScript
                 }
